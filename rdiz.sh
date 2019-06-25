0641c7b1852776b3206d29029d3a91f73ba73126
ZurelxlFang <49798038+ZurelxlFang@users.noreply.github.com>
2019-06-26 04:28:49 +0700

    Update rdiz.sh

--- rdiz.sh
+++ rdiz.sh
@@ -1,5 +1,113 @@
 #usr/bin/bash
 clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "SABAR MEMEK -";
+sleep 0.4;
+clear
+echo 'SABAR NJING!... \';
+sleep 1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
 bi='\033[34;1m' #biru
 i='\033[32;1m' #ijo
 pur='\033[35;1m' #purple
@@ -7,23 +115,35 @@ cy='\033[36;1m' #cyan
 me='\033[31;1m' #merah
 pu='\033[37;1m' #putih
 ku='\033[33;1m' #kuning
-echo
-echo
+echo $me"                 ██████    ██    ██"
+echo $me"                 ██  ██████████████"
+echo $pu"                 ████████  ████  ██"
+echo $pu"                 ██  ██████████████"
+echo $bi"[X]"$i "X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•"$bi"[X]"
+echo $pu" |" $me"["$pu" Author :"$me"]"$bi" DimasPnr"$pu"                            |"
+echo $pu" |" $me"["$pu" WhatsApp :"$me"]"$bi" 083873098030"$pu"                      |"
+echo $pu" |" $me"["$pu" THANKS TO :"$me"]"$bi"[Allah Swt] [Deray] [Faiz]"$pu"        |"
+echo $bi"[X]"$i "X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•"$bi"[X]"
 echo
 echo $pu"•"$bi"────────────────────"$pu"•"
 echo $i"|"$cy"   TERMUX TOOLS"  $i"    |"
 echo $pu"•"$bi"────────────────────"$pu"•"
-echo $me"["$pu" Author :"$me"]"$bi" DimasPnr"
-echo $me"["$pu" WhatsApp :"$me"]"$bi" 083873098030"
-echo $me"["$pu" THANKS TO :"$me"]"$bi"[Allah Swt] [Deray ] [Faiz]"
-echo $i"|"$pu" 1"$i" |"$cy"DarkFly"$i"|"
-echo
-echo $me"┌==="$bi"["$i"R.Diz"$bi"]"$me"======"$bi"["$i""Pilih Nomornya""$bi"]"
-echo $me"¦"
-read -p"└──# " pil
+echo $me"|"$pu"1"$me"|"$cy"["$cy"DarkFly""]"
+echo $me"|"$pu"2"$me"|"$cy"["$cy"DarkFBv6""]"
+echo $me"|"$pu"3"$me"|"$cy"["$cy"DarkFBv7""]"
+echo $me"|"$pu"4"$me"|"$cy"["$cy"VIRTEX""]"
+echo $me"|"$pu"5"$me"|"$cy"["$cy"Hack Facebook Massal""]"
+echo $me"|"$pu"6"$me"|"$cy"["$cy"WhoAreYou""]"$cy"("$pu"LACAK LOKASI DENGAN LI
+echo $me"|"$pu"7"$me"|"$cy"["$cy"BAJINGANv6""]"
+echo $me"|"$pu"8"$me"|"$cy"["$cy"ShellPhish""]"$cy"("$pu"MEMBUAT PHISING"$CY")
+echo $me"|"$pu"9"$me"|"$cy"["$cy"STARKV2""]"
+echo $me"|"$pu"10"$me"|"$cy"["$cy"INSTALL BAHAN BAHAN NJING""]"
+echo $me"|"$pu"0"$me"|"$cy"["$cy"KELUAR""]"
+echo $bi"["$cy"R.Diz"$bi"]"$bi"["$cy"Pilih Nomornya"$bi"]"
+echo $cy"╔"
+read -p"╚══>>" pil
 
-if [ $pil = 1 ]
-then
+if [ $pil = 1 ]                                                         
 clear
 figlet -f slant "N G O P I"|lolcat
 sleep 1
@@ -32,3 +152,106 @@ cd DarkFly-Tool
 python2 install.py
 DarkFly
 fi
+
+if [ $pil = 2 ]
+clear
+figlet -f slant "N G O P I"|lolcat
+sleep 1
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+python2 SETAN.py
+fi
+
+if [ $pil = 3 ]
+clear
+figlet -f slant "N G O P I"|lolcat
+sleep 1
+git clone git clone https://github.com/LOoLzeC/reverse_engineering
+cd reverse_engineering
+cd darkontol-v7
+python2 darkontol-v7.py
+fi
+
+if [ $pil = 4 ]
+clear
+figlet -f slant "N G O P I"|lolcat
+sleep 1
+git clone https://github.com/muhammadfathul/VIRTEX
+cd VIRTEX
+sh virtex.sh
+fi
+
+if [ $pil = 5 ]
+clear
+figlet -f slant "N G O P I"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+fi
+
+if [ $pil = 6 ]
+clear
+figlet -f slant "N G O P I"|lolcat
+sleep 1
+git clone https://github.com/FajarTheGGman/WhoAreYou
+cd WhoAreYou
+sh install.sh
+fi
+
+if [ $pil = 7 ]
+clear
+figlet -f slant "N G O P I"|lolcat
+sleep 1
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+cd BAJINGANv6
+sh BAJINGAN.sh
+fi
+
+if [ $pil = 8 ]
+clear
+figlet -f slant "N G O P I"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/shellphish
+cd shellphish
+sh shellphish.sh
+fi
+
+if [ $pil = 9 ]
+clear
+figlet -f slant "N G O P I"|lolcat
+sleep 1
+git clone https://github.com/aniketstark/STARK2.0
+cd STARK2.0
+python2 stark.py
+fi
+
+if [ $pil = 10 ]
+then
+clear\
+apt update && apt upgrade
+apt install python2
+apt install python2                                                                     pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem                                                                         gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " BERHASIL TUH GOBLOG"|lolcat
+fi
+
+if [ $pil = 0 ]
+then
+echo $me"TERIMA KASIH TELAH MENGGUNAKAN TOOL INI"                                       sleep 1
+echo $me"Author:R.Diz"
+exit
+fi
+
+
+
+
